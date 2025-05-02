b9ad194d7543331eb9d6bb944688b2bebeba76e0
djakimiuk <63128911+djakimiuk@users.noreply.github.com>
2025-05-02 03:31:27 +0200

    refactor: simplify MessageChannel implementation
    - Converted MessageChannel from a class to a plain object, streamlining the code structure and improving readability.

--- src/utils/message-channel.js
+++ src/utils/message-channel.js
@@ -1,14 +1,12 @@
-export class MessageChannel {
-  constructor() {
-    this.port1 = {
-      postMessage: () => {},
-      onmessage: null,
-    };
-    this.port2 = {
-      postMessage: () => {},
-      onmessage: null,
-    };
-  }
-}
+const MessageChannel = {
+  port1: {
+    postMessage: () => undefined,
+    onmessage: null,
+  },
+  port2: {
+    postMessage: () => undefined,
+    onmessage: null,
+  },
+};
 
 export default MessageChannel;
